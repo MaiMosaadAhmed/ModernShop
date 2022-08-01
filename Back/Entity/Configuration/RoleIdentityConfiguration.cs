b1539bb94beffacff678e0cd0c7f5cd43fb6d372
mai.mosaad.ahmed <mai.mosaad.ahmed@gmail.com>
2022-08-01 16:54:46 +0200

    Add Role Data

---
+++ Back/Entity/Configuration/RoleIdentityConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Entity.Core.authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Configuration
+{
+    class RoleIdentityConfiguration : IEntityTypeConfiguration<roleIdentity>
+    {
+        public void Configure(EntityTypeBuilder<roleIdentity> builder)
+        {
+            builder.HasData(
+                new roleIdentity
+                {
+                    Id = 1,
+                    Name = "SystemAdmin",
+                    NormalizedName = "SystemAdmin".ToUpper(),
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                }, new roleIdentity
+                {
+                    Id = 2,
+                    Name = "Supplier",
+                    NormalizedName = "Supplier".ToUpper(),
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                }, new roleIdentity
+                {
+                    Id = 3,
+                    Name = "Member",
+                    NormalizedName = "Member".ToUpper(),
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                }
+                );
+        }
+    }
+}
